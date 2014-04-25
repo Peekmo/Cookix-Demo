d7921d1e96abbe3dcfb7d1ae63ef657039d1eeed
Peekmo <axel.anceau@gmail.com>
2014-04-25 22:48:09 +0200

    Boot cleared

--- application/Boot.hx
+++ application/Boot.hx
@@ -1,34 +1,18 @@
 import cookix.core.http.request.Request;
-// import cookix.core.http.request.AbstractRequest;
-// import cookix.templating.TemplateManager;
-import cookix.core.container.ServiceContainer;
-import cookix.demo.services.DemoService;
-import cookix.core.routing.RoutingMacro;
-import cookix.core.services.routing.Routing;
 import cookix.core.Kernel;
 
 /**
  * Main of the application
+ * @author Axel Anceau (Peekmo)
  */
 class Boot
 {
+    /**
+     * Application's entry point
+     */
     public static function main()
     {
         var request = Request.create();
         Kernel.handle(request);
-        //var service : Routing = cast ServiceContainer.get('cookix.routing');
-        //trace(service.match('/test'));
-        //service.test();
-
-        // ServiceContainer.get('demo_service').test();
-
-        //var ctrl : cookix.demo.controllers.DemoController = new cookix.demo.controllers.DemoController();
-        // ctrl.testAction();
-
-        // var routes : cookix.tools.ObjectDynamic = RoutingMacro.getRoutes();
-        // trace(routes);
-
-        // var template : TemplateManager = new TemplateManager();
-        // template.render('templates/test.tpl');
     }
 }
