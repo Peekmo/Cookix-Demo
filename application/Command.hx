93f4c83c3611ccd232c17adcd7b805b52dfe81ac
Peekmo <axel.anceau@gmail.com>
2014-04-28 21:40:25 +0200

    Updating demo with new features

---
+++ application/Command.hx
@@ -0,0 +1,15 @@
+
+/**
+ * Entry point for Cookix's command line
+ * @author Axel Anceau (Peekmo)
+ */
+class Command
+{
+	/**
+	 * Entry point
+	 */
+	public static function main() : Void
+	{
+		
+	}
+}
